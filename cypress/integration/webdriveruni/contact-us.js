bf5637261095c6e217d63f4f30103a0645797b09
Rifqi Fadhilah <technipump@hotmail.com>
2021-11-11 16:26:03 +0700

    Added some assertion

--- cypress/integration/webdriveruni/contact-us.js
+++ cypress/integration/webdriveruni/contact-us.js
@@ -4,8 +4,12 @@ describe('Test Contact US from WebDriver Uni' , () => {
     it("should be able to submit a successful submission via contact us form",()=>{
         cy.viewport(1366,768)
         cy.visit("http://webdriveruniversity.com/Contact-Us/contactus.html")
-        // cy.visit("http://webdriveruniversity.com/")
-        // cy.get('#contact-us').click()
+        // Check if the page contains utf-8 characters
+        cy.document().should('have.property', 'charset').and('eq', 'UTF-8')
+        // Check if the title is WebDriver | Contact Us
+        cy.title().should('include','WebDriver | Contact Us')
+        // Check if the url is contact us
+        cy.url().should('include','contactus')
         cy.get('[name="first_name"]').type("John")
         cy.get('[name="last_name"]').type("Doe")
         cy.get('[name="email"]').type("test@test.com")
@@ -17,6 +21,9 @@ describe('Test Contact US from WebDriver Uni' , () => {
     it("should be able to reset a submission via contact us form",()=>{
         cy.viewport(1366,768)
         cy.visit("http://webdriveruniversity.com/Contact-Us/contactus.html")
+        cy.document().should('have.property', 'charset').and('eq', 'UTF-8')
+        cy.title().should('include','WebDriver | Contact Us')
+        cy.url().should('include','contactus')
         cy.get('[name="first_name"]').type("John")
         cy.get('[name="last_name"]').type("Doe")
         cy.get('[name="email"]').type("test@test.com")
@@ -26,12 +33,13 @@ describe('Test Contact US from WebDriver Uni' , () => {
         cy.get('[name="last_name"]').should('not.have.value','Doe')
         cy.get('[name="email"]').should('not.have.value','test@test.com')
         cy.get('textarea.feedback-input').should('not.have.value','This is a test on Cypress')
-
-
     });
     it("should not be able to submit a successful submission via contact us form as all fields are required",()=>{
         cy.viewport(1366,768)
         cy.visit("http://webdriveruniversity.com/Contact-Us/contactus.html")
+        cy.document().should('have.property', 'charset').and('eq', 'UTF-8')
+        cy.title().should('include','WebDriver | Contact Us')
+        cy.url().should('include','contactus')
         cy.get('[name="first_name"]').type("John")
         cy.get('[name="last_name"]').type("Doe")
         cy.get('[type="submit"]').click()
