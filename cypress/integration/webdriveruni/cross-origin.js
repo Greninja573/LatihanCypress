2e5eb48cc55a87c33cfbbcb416ac49fc56a6edcc
Rifqi Fadhilah <technipump@hotmail.com>
2021-11-12 16:41:29 +0700

    Added cross origin js

---
+++ cypress/integration/webdriveruni/cross-origin.js
@@ -0,0 +1,9 @@
+/// <reference types="Cypress" />
+
+it("should be able to access automationstore.com using link on webdriveruni",()=>{
+    cy.viewport(1366,768)
+    cy.visit("http://webdriveruniversity.com")
+    cy.document().should('have.property', 'charset').and('eq', 'UTF-8')
+    cy.get('#automation-test-store').invoke('removeAttr','target').click()
+
+});
